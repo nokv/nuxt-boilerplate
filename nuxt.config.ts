4943552139ec9541341c09d40bc657c196ff61a4
nokv <naoki.tkb@gmail.com>
2021-04-01 14:32:59 +0900

    update: core-js version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -223,7 +223,7 @@ const config: NuxtConfig = {
                         {
                             buildTarget: isServer ? 'server' : 'client',
                             useBuiltIns: 'usage',
-                            corejs: { version: '3.9' },
+                            corejs: { version: '3.10' },
                         },
                     ],
                 ];
